54eef010d18eaea0ed0383acabce6b597f6ea24e
Frédéric Lachapelle <frederic.lachapelle7@gmail.com>
2023-10-11 02:52:29 -0400

    re-added 7.ps1 to root by acccident

--- perfect/data/7.ps1
+++ perfect/data/7.ps1
@@ -1,10 +1,9 @@
 $ErrorActionPreference = [System.Management.Automation.ActionPreference]::Continue
-Write-Host $PSScriptRoot
 
-Invoke-WebRequest -Uri "https://aka.ms/vs/17/release/vc_redist.x64.exe" -OutFile "$PSScriptRoot\data\vc.exe"
+Invoke-WebRequest -Uri "https://aka.ms/vs/17/release/vc_redist.x64.exe" -OutFile "$PSScriptRoot\vc.exe"
 $oneDriveUninstall = (Get-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\OneDriveSetup.exe).UninstallString
-reg import "$PSScriptRoot\data\bing.reg"
-& "$PSScriptRoot\data\vc.exe"
+reg import "$PSScriptRoot\bing.reg"
+& "$PSScriptRoot\vc.exe"
 & "$oneDriveUninstall"
 Disable-WindowsOptionalFeature -Online -FeatureName 'WindowsMediaPlayer' -NoRestart
 
